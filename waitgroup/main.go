6c9003b24ca68824a339f08d082710db061985e4
isinov <isinov1995@gmail.com>
2021-04-03 05:11:50 +0300

    waitgroup

---
+++ waitgroup/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	wg := &sync.WaitGroup{}
+	months := []string{
+		"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"}
+
+	for i := 0; i < 3; i++ {
+	}
+	for _, month := range months {
+		wg.Add(1)
+		startWorker(month, wg) //кидаем дерьмо лопатами
+	}
+	wg.Wait()
+
+}
+
+func startWorker(cnt string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Println(cnt, " worked")
+}
